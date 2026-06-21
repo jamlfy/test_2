e592b228e2adc8583f9355eabf461e54068cf31a
jamlfy <j4mlfy@gmail.com>
2026-06-21 18:38:05 -0500

    Add suport to suwager

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -18,7 +18,14 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    customCssUrl: 'https://unpkg.com/swagger-ui-dist@5/swagger-ui.css',
+    customJs: [
+      'https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js',
+      'https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   await app.listen(3000);
   console.log('Backend running on http://localhost:3000');
